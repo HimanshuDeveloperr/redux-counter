bbc0ff6ad2373160c1323e8137da17c2ab57cf34
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-05-08 14:40:15 +0530

    task

--- src/Store/index.js
+++ src/Store/index.js
@@ -7,9 +7,21 @@ const CounterReducer = (state = { counter: 0 }, action) => {
     };
   }
 
+  if (action.type === "incrementby2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
+
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "decrementby2") {
+    return {
+      counter: state.counter - 2,
     };
   }
 
