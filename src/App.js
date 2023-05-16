8efd4f1247bce886cbc26c5ebe2a3c1542dfbd88
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-05-16 11:31:50 +0530

    Authentication Using redux store

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { Fragment } from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header'
 import Auth from './components/Auth'
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile'
+
 
 function App() {
+  const isAuth=useSelector(state=>state.auth.isLoggedIn);
+
   return (
     <Fragment>
     <Header/>
-    <Auth/>
+   {!isAuth
+    &&
+   <Auth/>
+   }
+   {isAuth && <UserProfile/>}
       <Counter />
     </Fragment>
   );
